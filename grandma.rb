3d5085842d4896b2e136fba718b5bb509afbe1b7
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-06-17 19:36:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA"
-    return "I LOVE YOU TOO PUMPKIN"
+  if phrase == 'I LOVE YOU GRANDMA'
+    return 'I LOVE YOU TOO PUMPKIN'
   end
   
 end
